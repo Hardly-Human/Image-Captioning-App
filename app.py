12abd264abde9f11597c1af1c904a8308afd9021
Shaik Rehanuddin <rehanuddin1024@gmail.com>
2020-11-17 18:01:18 +0530

    Added footer

--- app.py
+++ app.py
@@ -2,12 +2,22 @@ import streamlit as st
 from PIL import Image
 import requests
 
+def footer():
+	st.markdown("""
+	* * *
+	Built with ❤️ by [Rehan uddin](https://hardly-human.github.io/)
+	""")
+	st.success("Rehan uddin (Hardly-Human)👋😉")
+	st.markdown("### [Give Feedback](https://www.iamrehan.me/forms/feedback_form/feedback_form.html)\
+	 `            `[Report an Issue](https://www.iamrehan.me/forms/report_issue/report_issue.html)")
+
+
 def main():
   
 	st.title("Image Captioning App")
 	st.text("Built with Neural_Talk_2 API and Streamlit")
 	st.markdown("### [NeuralTalk 2 by Andrej Karpathy](https://github.com/karpathy/neuraltalk)\
-     `            `[![Open Source Love svg1](https://badges.frapsoft.com/os/v1/open-source.svg?v=103)](https://github.com/Hardly-Human/toonify-images)\
+     `            `[![Open Source Love svg1](https://badges.frapsoft.com/os/v1/open-source.svg?v=103)](https://github.com/Hardly-Human/Image-Captioning-App)\
 	`            `[![GitHub license](https://img.shields.io/github/license/Naereen/StrapDown.js.svg)](https://lbesson.mit-license.org/)")
 
 	image_file = st.file_uploader("Upload Image", type = ['jpg','png','jpeg'])
@@ -38,3 +48,4 @@ def main():
 
 if __name__== "__main__":
 	main()
+	footer()
